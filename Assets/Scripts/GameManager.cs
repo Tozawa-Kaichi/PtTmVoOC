f67d8ef78ae60643febb7986d5a7269584bc18fb
st37071556@icloud.com <st37071556@icloud.com>
2022-08-17 23:50:02 +0900

    mean while

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Tooltip等属性をうまく使って見やすいコードを
+//オブジェクト指向を意識しよう
+//カプセル化意識して無暗なパブリック禁止
 public class GameManager : MonoBehaviour
 {
 
